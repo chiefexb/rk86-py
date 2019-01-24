b88121a30339ca5e137ad7d717edda65de0ab44d
U-SIGMA\16717383 <16717383@SBT-SPB-IK4152.sigma.sbrf.ru>
2019-01-24 19:37:03 +0300

    1

--- ter.py
+++ ter.py
@@ -1,14 +1,21 @@
-import unicurses
-import locale
-
+import unicurses 
+import os, time
 stdscr = unicurses.initscr()
-stdscr.clear()
-cursor_x = 0
-cursor_y = 0
-for y in range (16):
-    for x in range (16):
-        stdscr.addstr(x, y, u'\u259A',               unicurses.A_REVERSE)
+pd = unicurses.newpad(10, 10)
+heigth=100
+width=100
 stdscr.refresh()
+st = ''
+pd.addstr(1,1, st)
+#pd.border(1)
+pd.box()
+pd.refresh( 0,0, 5,5, 20,75)
+for x in range(15):
+    time.sleep(1)
+    stdscr.refresh()
+    pd.refresh( 0,x, 5,5, 20,75)
+
+
 while True:
     c = stdscr.getch()
     if c == ord('q'):
