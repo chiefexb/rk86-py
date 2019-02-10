62c71bcb320889d795e559967b2c21dcc664560d
Aleksey Shilo <chief_exb@mail.ru>
2019-02-11 00:28:51 +0300

    1

--- ter.py
+++ ter.py
@@ -1,19 +1,25 @@
-import unicurses 
+from unicurses import *
 import os, time
-stdscr = unicurses.initscr()
-pd = unicurses.newpad(10, 10)
+UCS_DHLINE=chr(0x2550)
+UCS_DVLINE=chr(0x2551)
+
+stdscr = initscr()
+#pd = newpad(10, 10)
 heigth=100
 width=100
+stdscr.addch(0x2551)
 stdscr.refresh()
-st = ''
-pd.addstr(1,1, st)
+#st = unicurses.ACS_BOARD
+#pd.addchr(1,1, st)
 #pd.border(1)
-pd.box()
-pd.refresh( 0,0, 5,5, 20,75)
-for x in range(15):
-    time.sleep(1)
-    stdscr.refresh()
-    pd.refresh( 0,x, 5,5, 20,75)
+
+#int borderX(ls, rs, ts, bs, tl, tr, bl, br)  
+#wborder(pd,UCS_DVLINE,0 , 0, 0, 0, 0, 0, 0)  
+#pd.refresh( 0,0, 5,5, 20,75)
+#for x in range(15):
+#    time.sleep(1)
+#    stdscr.refresh()
+ #   pd.refresh( 0,x, 5,5, 20,75)
 
 
 while True:
